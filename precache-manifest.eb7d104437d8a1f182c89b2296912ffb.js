417c0681c690df84e7f94e79a575d4f1c10473f4
praveend <praveend2622@gmail.com>
2021-03-16 10:54:55 +0530

    Updates

---
+++ precache-manifest.eb7d104437d8a1f182c89b2296912ffb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf35e974f3077183414e66304273660",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "a261a3d79a7b3e6eb190",
+    "url": "/chess/static/css/main.25ebc687.chunk.css"
+  },
+  {
+    "revision": "dad5bc032aa46de40d5e",
+    "url": "/chess/static/js/2.463f4acf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chess/static/js/2.463f4acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a261a3d79a7b3e6eb190",
+    "url": "/chess/static/js/main.d5e4d8d1.chunk.js"
+  },
+  {
+    "revision": "fe54e1bce9cc9d1a3729",
+    "url": "/chess/static/js/runtime-main.bff3f106.js"
+  }
+]);
